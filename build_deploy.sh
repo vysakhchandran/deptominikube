65360064a1827cec00299012ffa0e2ace90113c4
Vantageboxllp <70579970+Vantageboxllp@users.noreply.github.com>
2021-10-13 02:50:35 +0530

    added installer script

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,7 +5,7 @@ echo "Starting image build......................."
 minikube ssh -- docker build -t flask_api_server /home/docker/deptominikube/ 
 echo "build completed with success."
 echo "Starting deployments on k8 cluster........."
-kubectl apply -f flask-api-deployment.yaml
+kubectl apply -f https://raw.githubusercontent.com/vysakhchandran/deptominikube/main/flask-api-deployment.yaml
 echo "Deploymet completed with success."
 kubectl get pods
 sleep 5
