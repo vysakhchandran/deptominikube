b381a3ae68a0430f4436ebccacf704d03c9e3840
Vantageboxllp <70579970+Vantageboxllp@users.noreply.github.com>
2021-10-13 13:47:46 +0530

    updated build script

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,11 +1,11 @@
-echo "Starting to clone the repo................."
-minikube ssh -- git clone https://github.com/vysakhchandran/deptominikube.git
-echo "Clone completed with success."
+#!/bin/bash
+echo "Moving the repo for build................."
+minikube ssh -- mkdir -p /home/docker/app
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $(minikube ssh-key) * docker@$(minikube ip):/home/docker/app/
 echo "Starting image build......................."
-minikube ssh -- docker build -t flask_api_server /home/docker/deptominikube/ 
-echo "build completed with success."
+minikube ssh -- docker build -t flask_api_server /home/docker/app/ 
 echo "Starting deployments on k8 cluster........."
-kubectl apply -f https://raw.githubusercontent.com/vysakhchandran/deptominikube/main/flask-api-deployment.yaml
+kubectl apply -f flask-api-deployment.yaml
 echo "Deploymet completed with success."
 kubectl get pods
 echo "Awaiting Ingress IP to be assigned...."
